5a8cb7c78af339859037a3d1281ab65e33098029
Camilo  Hernandez <camilo.hernandezmarin@jumbo.com>
2019-08-26 09:18:15 +0200

    sns and sqs using cloud templates

--- src/main/java/com/sample/msg/config/AwsCloudConfig.java
+++ src/main/java/com/sample/msg/config/AwsCloudConfig.java
@@ -1,24 +1,44 @@
 package com.sample.msg.config;
 
-import com.amazonaws.auth.*;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
+import org.springframework.cloud.aws.messaging.config.annotation.SnsConfiguration;
 import org.springframework.cloud.aws.messaging.config.annotation.SqsClientConfiguration;
 import org.springframework.cloud.aws.messaging.config.annotation.SqsConfiguration;
+import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
 import org.springframework.cloud.aws.messaging.listener.QueueMessageHandler;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @Import({SqsClientConfiguration.class, SqsConfiguration.class})
-public class AwsCloudConfig {
+class AwsCloudConfig {
+
+    @Bean
+    @Primary
+    AmazonSNS amazonSNS(@Value("${cloud.aws.region.static}") String region) {
+        return AmazonSNSClientBuilder.standard().
+                withRegion(region)
+                .build();
+    }
+
+    @Bean
+    @Primary
+    NotificationMessagingTemplate notificationMessagingTemplate(AmazonSNS amazonSNS) {
+        return new NotificationMessagingTemplate(amazonSNS);
+    }
 
     @Bean
     @Primary
     @Qualifier("amazonSQSAsync")
-    public AmazonSQSAsync amazonSQSAsync(@Value("${cloud.aws.region.static}") String region) {
+    AmazonSQSAsync amazonSQSAsync(@Value("${cloud.aws.region.static}") String region) {
         return AmazonSQSAsyncClientBuilder.standard()
                 .withRegion(region)
                 .build();
@@ -26,7 +46,7 @@ public class AwsCloudConfig {
 
     @Bean
     @Primary
-    public SimpleMessageListenerContainerFactory simpleMessageListenerContainerFactory(AmazonSQSAsync amazonSqs) {
+    SimpleMessageListenerContainerFactory simpleMessageListenerContainerFactory(AmazonSQSAsync amazonSqs) {
         SimpleMessageListenerContainerFactory factory = new SimpleMessageListenerContainerFactory();
         factory.setAmazonSqs(amazonSqs);
         factory.setMaxNumberOfMessages(1);
--- src/main/java/com/sample/msg/controller/SNSController.java
+++ src/main/java/com/sample/msg/controller/SNSController.java
@@ -5,30 +5,32 @@ import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sns.model.PublishRequest;
 import com.amazonaws.services.sns.model.PublishResult;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotNull;
+
 @Slf4j
 @RestController
+@RequiredArgsConstructor(onConstructor = @__({@Autowired, @NotNull}))
 public class SNSController {
 
+    private final NotificationMessagingTemplate notificationMessagingTemplate;
+    private final AmazonSNS amazonSNS;
+
     @Value("${sns.topic.ARN}")
-    String snsTopicDemoARN;
+    private String snsTopicDemoARN;
 
-    private AmazonSNS amazonSNS;
 
-    @Autowired
-    public SNSController() {
-        this.amazonSNS = AmazonSNSClientBuilder.standard().
-                withRegion(Regions.EU_CENTRAL_1)
-                .build();
-    }
+
 
     @RequestMapping(value = "/sendMessageQueue", method = RequestMethod.POST)
     public @ResponseBody
@@ -40,8 +42,10 @@ public class SNSController {
             log.info("===============================================\n");
 
             log.info("Sending a message to MyQueue.\n");
+            notificationMessagingTemplate.sendNotification(snsTopicDemoARN,
+                    "{\"mobileno\": \"+919996019206\", \"message\": \"Hello Camilo! How have you been doing? Hope you are doing great. Good bye!\"}", "subject");
             PublishRequest publishRequest = new PublishRequest(snsTopicDemoARN, notificationData, "test");
-            PublishResult publishResult = this.amazonSNS.publish(publishRequest); ;
+            PublishResult publishResult = this.amazonSNS.publish(publishRequest);
             log.info("Message Sent.\n");
 
         } catch (final Exception ase) {
